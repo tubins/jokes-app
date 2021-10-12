fc73f8ac6f941ed1cce549bc1881835b41b98b42
tubins <tubin.shaji@gmail.com>
2021-10-12 22:51:58 +0100

    Add missing annotation

--- src/main/java/com/tubz/jokesapp/services/JokesServiceImpl.java
+++ src/main/java/com/tubz/jokesapp/services/JokesServiceImpl.java
@@ -1,6 +1,7 @@
 package com.tubz.jokesapp.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,6 +9,7 @@ public class JokesServiceImpl implements JokesService {
 
     private ChuckNorrisQuotes chuckNorrisQuotes;
 
+    @Autowired
     public JokesServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
         this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
